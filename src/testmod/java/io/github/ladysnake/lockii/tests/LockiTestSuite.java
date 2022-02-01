0e6b548dfdf52a9224ed606ef1fd697c72f4c84b
Pyrofab <redstoneinfire@gmail.com>
2022-02-01 10:47:09 +0100

    Fix *interface injection

--- src/testmod/java/io/github/ladysnake/lockii/tests/LockiTestSuite.java
+++ src/testmod/java/io/github/ladysnake/lockii/tests/LockiTestSuite.java
@@ -17,40 +17,52 @@
  */
 package io.github.ladysnake.lockii.tests;
 
-import io.github.ladysnake.elmendorf.GameTestUtil;
 import io.github.ladysnake.locki.DefaultInventoryNodes;
 import io.github.ladysnake.locki.InventoryLock;
 import io.github.ladysnake.locki.Locki;
+import io.github.ladysnake.locki.impl.LockiComponents;
 import io.github.ladysnake.lockii.Lockii;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
+
+import static io.github.ladysnake.elmendorf.ByteBufChecker.any;
 
 public class LockiTestSuite implements FabricGameTest {
     public static final InventoryLock lock = Locki.registerLock(Lockii.id("test_suite"));
 
     @GameTest(structureName = EMPTY_STRUCTURE)
     public void lockingPreventsItemPickup(TestContext ctx) {
-        PlayerEntity mockPlayer = GameTestUtil.spawnPlayer(ctx, 1, 0, 1);
-        mockPlayer.getInventory().addLock(lock, DefaultInventoryNodes.INVENTORY);
+        ServerPlayerEntity player = ctx.spawnServerPlayer(1, 0, 1);
+        player.getInventory().addLock(lock, DefaultInventoryNodes.INVENTORY);
         ctx.spawnItem(Items.DIAMOND, 1, 0, 2);
         ctx.expectEntity(EntityType.ITEM);
-        ctx.waitAndRun(20, () -> {
-            ctx.expectEntity(EntityType.ITEM);
-            ctx.complete();
-        });
+        player.playerTick();
+        ctx.expectEntity(EntityType.ITEM);
+        ctx.complete();
     }
 
     @GameTest(structureName = EMPTY_STRUCTURE)
     public void itemPickupWorks(TestContext ctx) {
-        GameTestUtil.spawnPlayer(ctx, 1, 0, 1);
-        ctx.spawnItem(Items.REDSTONE, 1, 0, 2);
-        ctx.waitAndRun(20, () -> {
-            ctx.dontExpectEntity(EntityType.ITEM);
-            ctx.complete();
-        });
+        ServerPlayerEntity player = ctx.spawnServerPlayer(1, 0, 1);
+        ctx.spawnItem(Items.REDSTONE, 1, 0, 1);
+        ctx.expectEntity(EntityType.ITEM);
+        player.playerTick();
+        ctx.dontExpectEntity(EntityType.ITEM);
+        ctx.complete();
+    }
+
+    @GameTest(structureName = EMPTY_STRUCTURE)
+    public void componentGetsSynced(TestContext ctx) {
+        ServerPlayerEntity player = ctx.spawnServerPlayer(1, 0, 1);
+        player.getInventory().addLock(lock, DefaultInventoryNodes.INVENTORY);
+        ctx.verifyConnection(player, conn -> conn.sentEntityComponentUpdate(player, LockiComponents.INVENTORY_KEEPER, c -> c.checkVarInt(any())
+                .checkString(any())
+                .checkVarInt(1)
+                .checkByte((byte)(1 << lock.getRawId()))));
+        ctx.complete();
     }
 }
